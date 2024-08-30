5c9a2f07c378ef9b629d65a191609ac52c9b8fd1
ToiTenSon <23010313@st.phenikaa-uni.edu.vn>
2024-08-30 11:34:14 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/app/Recursion.java
@@ -0,0 +1,8 @@
+public class Recursion {
+    long factorial( long number) {
+        if (number <= 1) // base case
+        return 1;
+        else
+        return number * factorial(number - 1);
+       }
+}
