1daa98770bcd90e1b351edaa0e934fcbccc315a2
ToiTenSon <23010313@st.phenikaa-uni.edu.vn>
2024-09-19 11:13:32 +0700

    Add files via upload

---
+++ week4/testArithmetic/testArithmetic/Const.java
@@ -0,0 +1,5 @@
+public class Const extends Node {
+    private double value;
+    public Const(double d) { value = d; }
+    public double eval() { return value; }
+   }
---
+++ week4/testArithmetic/testArithmetic/Plus.java
@@ -0,0 +1,9 @@
+
+public class Plus extends Binop {
+    public Plus(Node l, Node r) {
+    super(l, r);
+    }
+    public double eval() {
+    return lChild.eval() + rChild.eval();
+    }
+}
