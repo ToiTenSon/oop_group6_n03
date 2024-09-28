4407ac1e6f8943abd2a01539f69c7889a7e2f957
ToiTenSon <23010313@st.phenikaa-uni.edu.vn>
2024-09-28 18:56:56 +0000

    update

---
+++ deploy/FinalProject/src/CourseManager.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseManagement {
+    private List<Course> courses = new ArrayList<>();
+
+    public void addCourse(Course course) {
+    }
+
+    public void removeCourse(String courseId) {
+    }
+
+    public void updateCourse(Course course) {
+    }
+
+    public List<Course> getListCourse() {
+        return courses;
+    }
+}
---
+++ deploy/FinalProject/src/StudentManager.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManagement {
+    private List<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student) {
+    }
+
+    public void removeStudent(int student_id) {
+    }
+
+    public void updateStudent(Student student) {
+    }
+
+    public void addScore(Student student, Course course, float grade) {
+    }
+
+    public Student getStudent(int id) {
+        return null;
+    }
+
+    public Course getCourseAvg(int minAvg) {
+        return null;
+    }
+
+    public List<Student> displayRegisteredCourses() {
+        return null;
+    }
+
+    public float getAverageGrade() {
+        return 0.0f;
+    }
+
+    public void requestAddScore(List<Student> students) {
+    }
+
+    public boolean isValidScore() {
+        return false;
+    }
+}
